c8a44ad71aa2d4aac28a93480a6d1e95a74e8181
仙逝丶烟尘 <zh616110538@gmail.com>
2017-06-22 10:53:54 +0800

    no message

--- linux实用操作/shell.sh
+++ linux实用操作/shell.sh
@@ -6,4 +6,5 @@ sudo arpspoof -i eth0 -t 192.168.1.110 192.168.1.1
 sudo arpspoof -i eth0 -t 192.168.1.1 192.168.1.110
 sudo driftnet -i eth0
 
-
+#查看内核版本
+awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg
