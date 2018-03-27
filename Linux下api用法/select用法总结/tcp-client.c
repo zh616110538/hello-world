05473ab9a52593af54416f7831d25f4caa365f0c
仙逝丶烟尘 <zh616110538@gmail.com>
2018-03-27 14:00:02 +0800

    Update tcp-client.c

--- Linux下api用法/select用法总结/tcp-client.c
+++ Linux下api用法/select用法总结/tcp-client.c
@@ -40,6 +40,7 @@ int main(int argc,char *argv[])
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(SERVPORT);
   serv_addr.sin_addr = *((struct in_addr *)host->h_addr);
+  //inet_pton(AF_INET,"127.0.0.1",(void *)&serv_addr.sin_addr);
   bzero(&(serv_addr.sin_zero),8);
 
   if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(struct sockaddr)) ==-1)
