b07e38e93a5993ca78892a15405222dbb937447b
zhu <>
2019-05-09 10:49:40 +0800

    C++11打印时间

---
+++ C语言编程一些技巧/c++11记录时间/costtime.cpp
@@ -0,0 +1,15 @@
+#include <chrono>   
+using namespace std;
+using namespace chrono;
+
+int main()
+{
+	auto start = system_clock::now();
+	// do something...
+	auto end   = system_clock::now();
+	auto duration = duration_cast<microseconds>(end - start);
+	cout <<  "花费了" 
+		 << double(duration.count()) * microseconds::period::num / microseconds::period::den 
+		 << "秒" << endl;
+	 return 0;
+}
